a53a90c1c85f6fb741572281ffab1b69f8804d35
Andres Ruiz <andrsruiz@gmail.com>
2020-04-30 11:16:34 -0500

    Add Math Functions

---
+++ js/calculator.js
@@ -0,0 +1,23 @@
+const ZERO = 0;
+const ONE = 1;
+const TWO = 2;
+const THREE = 3;
+const FOUR = 4;
+const FIVE = 5;
+const SIX = 6;
+const SEVEN = 7;
+const EIGHT = 8;
+const NINE = 9;
+const POINT = '.';
+const PLUS = '+';
+const MINUS = '-';
+const MULTIPLY = '*';
+const DIVIDE = '/';
+const EQUAL = '=';
+const PERCENTAGE = '%';
+
+/* MATH FUNCTIONS */
+const add = function(x, y){ return x + y; }
+const subtract = function(x, y){ return x - y; }
+const multiply = function(x, y){ return x * y; }
+const divide = function(x, y){ return x / y; }
